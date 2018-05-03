da5ce3147c5c86e31724b64de03136ed57d82dc3
Piervincenzo Parisi <piervi_parisi@yahoo.it>
2018-05-04 00:10:05 +0200

    Header

---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="page-header header-filter clear-filter purple-filter" data-parallax="true" >
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 ml-auto mr-auto">
+                        <div className="brand">
+                            <h1>SlowShow.org</h1>
+                            <h3>The National Italian Fansite</h3>
+                            <h6>Coming soon...</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
